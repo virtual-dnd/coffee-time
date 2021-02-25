db4c2141dcbb8484cf1e46449c51031924b94abe
Casey Baggz <caseybaggz@gmail.com>
2021-02-24 19:05:28 -0600

    chore: clean up files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {StrictMode} from 'react';
+import {render} from 'react-dom';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 );
 
